37698904b0e14ba2c613c2c58f78b956cd5009c0
calexanderberg <carlalexanderberg@gmail.com>
2024-07-24 18:22:17 +0200

    fixes

--- website/main.js
+++ website/main.js
@@ -3,7 +3,7 @@ document.addEventListener('keydown', function (event) {
     case '0': window.location.href = '/'; break;
     case '1': window.location.href = '/about'; break;
     case '2': window.location.href = '/showcase'; break;
-    case '3': window.location.assign('https://blog.calexanderberg.com'); break;
+    case '3': window.location = 'https://blog.calexanderberg.com'; break;
     case '4': window.location.href = '/contact'; break;
     default: break;
   }
